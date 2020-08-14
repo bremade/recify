f8f290ea28f0ee4e8c7ebe9632685489a6ab1691
Jan Bremauer <bremauer.jan2@gmail.com>
2020-08-14 13:47:55 +0200

    REC-3: Implemented example page with material components

---
+++ frontend/src/components/Navbar.svelte
@@ -0,0 +1,23 @@
+<script>
+    import TopAppBar, {Row, Section, Title} from '@smui/top-app-bar';
+    import IconButton from '@smui/icon-button';
+
+    let prominent = false;
+    let dense = false;
+    let secondaryColor = false;
+    export let drawerOpen = false;
+</script>
+
+<TopAppBar variant="static" {prominent} {dense} color={secondaryColor ? 'secondary' : 'primary'}>
+    <Row>
+        <Section>
+        <IconButton class="material-icons" on:click={() => drawerOpen = !drawerOpen}>menu</IconButton>
+        <Title>Recify</Title>
+        </Section>
+        <Section align="end" toolbar>
+            <IconButton class="material-icons" aria-label="Share">share</IconButton>
+            <IconButton class="material-icons" aria-label="Like">favorite</IconButton>
+            <IconButton class="material-icons" aria-label="Bookmark this page">bookmark</IconButton>
+        </Section>
+    </Row>
+</TopAppBar>
