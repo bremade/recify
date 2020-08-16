cca0a9d45fe1c6f3d93adf17c63e9f94bede0879
matthias Rupp <matthias.rupp@posteo.de>
2020-08-16 12:44:29 +0200

    REC-16: Reformat some files from tabs to spaces

--- persistence/mongo.go
+++ persistence/mongo.go
@@ -3,66 +3,66 @@ package persistence
 import (
     "context"
     "fmt"
-	"time"
+    "time"
 
-	"github.com/bremade/recify/model"
-	"go.mongodb.org/mongo-driver/bson"
+    "github.com/bremade/recify/model"
+    "go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type DBError struct {
-	message string
+    message string
 }
 
 func (err DBError) Error() string {
-	return err.message
+    return err.message
 }
 
 type DB struct {
-	dbClient *mongo.Client
-	database *mongo.Database
+    dbClient *mongo.Client
+    database *mongo.Database
 }
 
 func (db *DB) Open(uri string) error {
-	clientOptions := options.Client().ApplyURI(uri)
+    clientOptions := options.Client().ApplyURI(uri)
 
-	client, err := mongo.NewClient(clientOptions)
-	if err != nil {
-		fmt.Println("error creating mongodb client", err)
-		return err
-	}
+    client, err := mongo.NewClient(clientOptions)
+    if err != nil {
+        fmt.Println("error creating mongodb client", err)
+        return err
+    }
 
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		fmt.Println("error connecting to mongodb", err)
-		return err
-	}
+    ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+    err = client.Connect(ctx)
+    if err != nil {
+        fmt.Println("error connecting to mongodb", err)
+        return err
+    }
 
-	db.dbClient = client
+    db.dbClient = client
 
-	fmt.Println("mongodb opened")
-	return nil
+    fmt.Println("mongodb opened")
+    return nil
 }
 
 func (db *DB) Setup(){
-	db.database = db.dbClient.Database("recify")
+    db.database = db.dbClient.Database("recify")
 }
 
 func (db *DB) QueryTest(id int) (model.Test, error) {
-	collection := db.database.Collection("Test")
-	var result model.Test
+    collection := db.database.Collection("Test")
+    var result model.Test
 
     ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err := collection.FindOne(ctx, bson.D{
-		{Key: "id", Value: id},
-	}).Decode(&result)
+    err := collection.FindOne(ctx, bson.D{
+        {Key: "id", Value: id},
+    }).Decode(&result)
 
-	fmt.Println("result:", result)
+    fmt.Println("result:", result)
 
-	return result, err
+    return result, err
 }
 
 func (db *DB) CreateUser(user model.User) error {
@@ -89,9 +89,9 @@ func (db *DB) CheckUser(name string, passwordHash string) (string, error) {
 
     ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
     err := collection.FindOne(ctx, bson.D{
-		{Key: "name", Value: name},
+        {Key: "name", Value: name},
         {Key: "passwordhash", Value: passwordHash},
-	}).Decode(&user)
+    }).Decode(&user)
 
     if err != nil {
         return "", err
@@ -105,8 +105,8 @@ func (db *DB) ContainsUsername(name string) bool {
 
     ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
     cnt, _ := collection.CountDocuments(ctx, bson.D{
-		{Key: "name", Value: name},
-	})
+        {Key: "name", Value: name},
+    })
 
     return cnt > 0
 }
--- persistence/persistence.go
+++ persistence/persistence.go
@@ -1,13 +1,13 @@
 package persistence
 
 import (
-	"github.com/bremade/recify/model"
+    "github.com/bremade/recify/model"
 )
 
 type Persistence interface {
-	Open(uri string) error
-	Setup() error
-	QueryTest(id int) model.Test
+    Open(uri string) error
+    Setup() error
+    QueryTest(id int) model.Test
 
     CreateUser(user model.User) error
     CheckUser(name string, passwordHash string) (string, error)
