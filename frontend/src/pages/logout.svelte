b886f432cdb089c3786aa212c05f41ef16e35911
matthias Rupp <matthias.rupp@posteo.de>
2020-08-19 20:34:48 +0200

    REC-21: Demo and /logout

---
+++ frontend/src/pages/logout.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Card from '@smui/card';
+  import Button from '@smui/button';
+
+  function logout() {
+    fetch('/api/v1/auth/logout', {
+      method: 'POST'
+    }).then(result => {
+      // This should never fail
+      if (result.status === 200) {
+        window.location.pathname = '/';
+      }
+    });
+  }
+</script>
+
+<div class="login-container">
+  <div class="login">
+    <Card variant="outlined">
+      <div class="title">
+        Logout
+      </div>
+      <p>
+        Are you sure?
+      </p>
+      <div class="input-container">
+        <Button class="logout-button" variant="unelevated" on:click={logout}>Logout</Button>
+      </div>
+    </Card>
+  </div>
+</div>
