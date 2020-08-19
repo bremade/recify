05333631e557c78399baa28aec883a3ed37fd67b
Jan Bremauer <bremauer.jan2@gmail.com>
2020-08-19 13:47:55 +0200

    REC-17: Implemented routify client side routing

---
+++ frontend/src/pages/_fallback.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { url } from "@sveltech/routify";
+    import Button, {Label} from '@smui/button';
+</script>
+
+<div id="notfound">
+    <div class="notfound">
+        <div class="error404">
+            <h3 class="notfoundh3">
+            Oops! Page not found
+            </h3>
+            <h1 class="notfoundh1">
+                <span>404</span>
+            </h1>
+        </div>
+        <h2 class="notfoundh2">
+            im sorry, but the page you requested was not found
+        </h2>
+        <Button class="notfoundbutton" href={$url("/")} variant="raised">
+            <Label>Home Page</Label>
+        </Button>
+    </div>
+</div>
