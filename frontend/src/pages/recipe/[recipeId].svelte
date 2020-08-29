381e856f98f9275a53d0503841adc23bb691042f
matthias Rupp <matthias.rupp@posteo.de>
2020-08-29 17:50:17 +0200

    REC-23: Recipe detail view finished

--- frontend/src/pages/recipe/[recipeId].svelte
+++ frontend/src/pages/recipe/[recipeId].svelte
@@ -5,36 +5,32 @@
   import Navbar from '../../components/Navbar.svelte';
 
   // Extract recipe-id from route
-  const id = params.recipeId;
+  const id = $params.recipeId;
 
-  const recipe = {
-    _id: id,
-    title: 'Tomato soup',
-    servings: 4,
+  let recipe = {
+    id: id,
+    title: '',
+    servings: 0,
     time: {
-      cooktime: 160,
-      worktime: 100,
-      resttime: 100
+      cooktime: 0,
+      worktime: 0,
+      resttime: 0
     },
-    ingredients: [
-      { amount: 1, unit: 'L', ingredient: 'Milk' },
-      { amount: 5, unit: '', ingredient: 'Tomatoes' },
-      { amount: 1, unit: '', ingredient: 'Onion' },
-      { amount: 2, unit: 'g', ingredient: 'Salt' },
-      { amount: 500, unit: 'ml', ingredient: 'Water' }
-    ],
-    steps: [
-      { description: 'Cut tomatoes' },
-      { description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.' },
-      { description: 'Cook tomatoes for at least 10 minutes' },
-      { description: 'Serve with an onion' }
-    ],
-    price: 1.5,
-    description: "A delious soup",
-    tags: ['Delicious', 'Soup', 'veggy']
+    ingredients: [],
+    steps: [],
+    price: 0,
+    description: '',
+    tags: []
   };
 
-  var servings = recipe.servings;
+  let servings = recipe.servings;
+
+  fetch('/api/v1/recipe/' + id)
+  .then(resp => resp.json())
+  .then(resp => {
+    recipe = { ...recipe, ...resp };
+    servings = recipe.servings;
+  });
 
   function formatTime(minutes) {
     if (minutes < 60) {
@@ -76,7 +72,9 @@
           </Col>
         </Row>
       </Container>
-      <img class="recipe-image" src="/placeholder.webp">
+      {#if recipe.picture }
+        <img class="recipe-image" src={`/pictures/${recipe.picture}`}>
+      {/if}
     </Card>
     <Row class="mt-4">
       <Col md={4}>
@@ -113,8 +111,8 @@
             <table class="ingredients">
               {#each recipe.ingredients as ingredient, index}
                 <tr class={index % 2 == 0 ? 'highlighted' : ''}>
-                  <td class="amount">{(ingredient.amount / recipe.servings) * servings}{ingredient.unit}</td>
-                  <td class="ingredient">{ingredient.ingredient}</td>
+                  <td class="amount">{(ingredient.amount / recipe.servings) * servings} {ingredient.unit}</td>
+                  <td class="ingredient">{ingredient.name}</td>
                   </tr>
               {/each}
             </table>
