ed316b028e70f4e8f1ca0649f776985f69d4bef6
Jan Bremauer <bremauer.jan2@gmail.com>
2020-09-17 12:42:18 +0200

    REC-20: Added first recipe layout and card component

---
+++ frontend/src/components/RecipeCard.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Card, {Content, PrimaryAction, Media, MediaContent, Actions, ActionButtons, ActionIcons} from '@smui/card';
+  import { Container, Row, Col } from 'sveltestrap';
+  
+  let imageSrc = '/testPic.jpg';
+
+  export let recipeId;
+</script>
+<a href="/recipe/{recipeId}">
+  <Card>
+      <img class="cardImage" src={imageSrc} alt={recipeId}/>
+      <Content class="cardContent">
+        <h3 style="margin: 0 0 10px;">{recipeId}</h3>
+        <Row>
+          <Col align="left">
+            Time
+          </Col>
+          <Col align="center">
+            Price
+          </Col>
+          <Col align="right">
+            Rating
+          </Col>
+        </Row>
+      </Content>
+  </Card>
+</a>
--- frontend/src/pages/index.svelte
+++ frontend/src/pages/index.svelte
@@ -1,9 +1,27 @@
 <script>
   import Filler from '../components/Filler.svelte';
   import Navbar from '../components/Navbar.svelte';
+  import RecipeCard from '../components/RecipeCard.svelte';
+  import { Container, Row, Col, Table } from 'sveltestrap';
+
+  let topRecipes = ["Das ist ein unglaublich langer text um den umbruch zu testen", "Test2", "Der Text hier ist noch länger und geht über ganze 3 Zeilen statt nur 2 wuhuuu wuhuuuuu", "Test4", "Test5", "Test6", "Test7", "Test8"]
 </script>
 
-<div>
-  <Navbar />
-  <Filler/>
-</div>
+<Navbar/>
+<main id="content">
+  <Container class="contentContainer">
+    <Row>
+      <h2>Top Recipes</h2>
+    </Row>
+    <Row id="recipeCards">
+      {#each topRecipes as topRecipe}
+        <Col sm="12" md="4" xl="3" class="gridLayout">
+          <RecipeCard recipeId={topRecipe}/>
+        </Col>
+      {/each}
+    </Row>
+    <Row>
+      <Filler/>
+    </Row>
+  </Container>
+</main>
